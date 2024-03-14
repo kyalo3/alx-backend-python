5115043694b8b2a0e4e962312204ace781ca2c5e
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 23:24:26 +0300

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,11 @@
-def safely_get_value(dct, key, default = None):
+#!/usr/bin/env python3
+"""function add type annotations"""
+from typing import Callable, Iterator, Union, Optional, Sequence, Any, TypeVar
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """return values"""
     if key in dct:
         return dct[key]
     else:
