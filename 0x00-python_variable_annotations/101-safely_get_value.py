ce2812e7320f266df870be03289c8bc6192dc10b
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 23:28:57 +0300

    11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,11 @@ from typing import Callable, Iterator, Union, Sequence, Any, TypeVar, Mapping
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
     """return values"""
     if key in dct:
         return dct[key]
