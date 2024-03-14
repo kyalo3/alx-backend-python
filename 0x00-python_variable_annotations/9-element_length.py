f5d077085f4dceb5849266418064095bb8432e4b
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 17:25:15 +0300

    9. Let's duck type an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
+"""function’s parameters and return values with the appropriate types"""
+from typing import Iterable, Sequence, List, Tuple
 
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
--- 0x00-python_variable_annotations/9-main.py
+++ 0x00-python_variable_annotations/9-main.py
@@ -1,4 +1,4 @@
-8-make_multiplier.py#!/usr/bin/env python3
+#!/usr/bin/env python3
 
 element_length =  __import__('9-element_length').element_length
 
