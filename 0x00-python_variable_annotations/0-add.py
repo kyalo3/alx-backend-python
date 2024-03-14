557589912397ca70fe7a314a37c38037c0573f8c
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 12:40:21 +0300

    0. Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""function add that takes a float a and a float b as arguments"""
+
 
 def add(a: float, b: float) -> float:
+    """eturns their sum as a float"""
     return a + b
