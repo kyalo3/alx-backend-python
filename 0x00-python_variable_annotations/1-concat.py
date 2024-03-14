2fe9f3e3fd216a6d41f843800d533a82d0a757c4
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 12:44:47 +0300

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
+"""function concatenates that takes a string str1 and a string str2 as arguments"""
 
+
+def concat(str1: str, str2: str) -> str:
+    """returns their sum as a string"""
+    return str1 + str2
