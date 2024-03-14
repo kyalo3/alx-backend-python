6aca1f671ee560bab577d76e590d92a180aba000
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-14 13:08:39 +0300

    2. Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 
+from typing import Callable, Iterator, Union, Optional
+
+"""function floor that takes a float n as an arg"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    return int(n)
