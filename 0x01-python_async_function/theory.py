f53a9de9c4fed50836c8dd3a67fa5f483a3fabd8
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-19 12:22:47 +0300

    functions

--- 0x01-python_async_function/theory.py
+++ 0x01-python_async_function/theory.py
@@ -1,11 +1,24 @@
+from random import randint
+import time
+import asyncio
+
+
 def odds(start, stop):
+    """generator function(odds)"""
     for odd in range(start, stop + 1, 2):
+        """used to pause and resume the fucntion"""
         yield odd
 
+def randn():
+    """randn function to illustrate coroutines"""
+    time.sleep(3)
+    return randint(1, 10)
+
 def main():
-    odd_values = [odd for odd in odds(3, 45)]
-    odds2 = list(odds(21, 45))
-    print(odd_values)
+    """returns the diff lists"""
+    odds1 = [odd for odd in odds(3, 45)]
+    odds2 = tuple(odds(21, 45))
+    print(odds1)
     print(odds2)
 
 if __name__ == "__main__":
