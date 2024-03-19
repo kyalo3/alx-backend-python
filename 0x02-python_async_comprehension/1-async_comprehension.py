af8299631f8492f81cd257cfdc71782e63be4024
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-20 01:00:25 +0300

    1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
-""" """
+""" coroutine called async_generator that takes no arguments """
 import asyncio
-from typing import Generator
+from typing import AsyncGenerator
+import random
+from random import uniform
+from typing import List
 
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    return [i async for i in async_generator()][:10]
+
+
+async def main():
+    print(await async_comprehension())
+
+asyncio.run(main())
