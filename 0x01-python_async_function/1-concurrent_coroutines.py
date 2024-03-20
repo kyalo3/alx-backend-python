c7ffab2b47d67c87ac76c141d7fd3871ccf0e48c
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-20 15:43:07 +0300

    new chanes on task 1 file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 """an asynchronous coroutine that takes in an integer argument"""
 import asyncio
-import random
 from random import uniform
 from typing import List
+from typing import AsyncGenerator
 from _basic_async_syntax import wait_random
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """async function that waits for a random delay"""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def wait_n(n: int, max_delay: int) -> float:
-    """async function that waits for a random delay"""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
     return sorted(delays)
